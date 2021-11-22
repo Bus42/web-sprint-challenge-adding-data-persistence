f2b6bb94826359b59f71c102c5e4f874748b415f
bus42 <gbrewton1980@gmail.com>
2021-11-22 01:20:44 -0700

    WIP passes manual testing, but fails 13, 14, 17

--- api/task/model.js
+++ api/task/model.js
@@ -2,12 +2,15 @@ const db = require('../../data/dbConfig');
 
 async function find() {
     try {
-        const tasks = await db('tasks');
+        const tasks = await db('tasks as t')
+            .leftJoin('projects as p', 't.project_id', 'p.project_id')
+            .select('t.task_id', 't.task_description', 't.task_notes')
         return tasks.map(task => {
-            return { ...task, task_completed: task.task_completed ? true : false };
-        });
+            return { ...task, task_completed: task.task_completed ? true : false, project_completed: task.project_completed ? true : false };
+        })
     } catch (error) {
-        console.log({ ...error, message: "error fetching tasks" });
+        console.log({ ...error, message: "error finding task" });
+        return error
     }
 }
 
