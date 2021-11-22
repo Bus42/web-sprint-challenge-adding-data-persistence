19e695c9b4bf627db1655f58e2c4ec3d35210461
bus42 <gbrewton1980@gmail.com>
2021-11-21 23:05:12 -0700

    feat(POST): /api/tasks
    returns newly created task

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,21 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig');
+
+async function find() {
+    try {
+        return await db('tasks');
+    } catch (error) {
+        console.log({ ...error, message: "error fetching tasks" });
+    }
+}
+
+async function insert(task) {
+    // insert task into tasks table then return newly created task
+    db.insert(task).into('tasks')
+    return db('tasks')
+        .where('task_description', task.task_description).first();
+}
+
+module.exports = {
+    find,
+    insert
+}
