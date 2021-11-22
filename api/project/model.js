e00c9f4e4ad67f38a5cbff7f9f59c0285b3973ed
bus42 <gbrewton1980@gmail.com>
2021-11-22 00:01:58 -0700

    feat(POST): /api/projects
    returns newly created project

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,25 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig');
+
+async function find() {
+    try {
+        return await db('projects');
+    } catch (error) {
+        console.log({ ...error, message: "error fetching projects" });
+        return error
+    }
+}
+
+async function insert(project) {
+    try {
+        const [id] = await db('projects').insert(project);
+        return await db('projects').where({ project_id: id }).first();
+    } catch (error) {
+        console.log({ ...error, message: "error inserting project" });
+        return error
+    }
+}
+
+module.exports = {
+    find,
+    insert
+}
